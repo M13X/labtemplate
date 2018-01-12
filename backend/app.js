ad2ae5b545b4adcf9c8e06102a61cd59baacebf1
dimoftebogdanandrei@gmail.com <dimoftebogdanandrei@gmail.com>
2018-01-12 13:33:30 +0200

    +Added a new component +Added a lite database of pets

--- backend/app.js
+++ backend/app.js
@@ -25,8 +25,10 @@ app.use(cookieParser());
 // Controllers
 var index = require('./src/rest/index');
 var user = require('./src/rest/user');
+var pets = require('./src/rest/pets');
 app.use('/api', index);
 app.use('/api/user', user);
+app.use('/api/pets', pets);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
